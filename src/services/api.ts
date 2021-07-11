09ed45598a1fccfe020ef0a9aee5d3a63f62f4e2
heberleonard2 <heber.leonard2@gmail.com>
2021-07-10 21:55:49 -0300

    chore: config axios baseURL

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,7 +4,10 @@ import { parseCookies } from 'nookies'
 const { 'solides.token': token } = parseCookies()
 
 export const api = axios.create({
-  baseURL: process.env.API_ENDPOINT
+  baseURL:
+    process.env.NODE_ENV === 'development'
+      ? 'localhost:3000'
+      : process.env.API_ENDPOINT
 })
 
 if (token) {
